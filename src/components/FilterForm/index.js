c77f506bdf9f6ada0fd64c7f6cd0b3b3255e5f87
Oleksiy Pokormyakho <116438996+OleksiyPok@users.noreply.github.com>
2023-12-01 13:27:56 +0100

    comments have been deleted

--- src/components/FilterForm/index.js
+++ src/components/FilterForm/index.js
@@ -1,6 +1,1 @@
-import FilterForm from './FilterForm';
-export default FilterForm;
-
-// export { default } from './FilterForm';
-
-// export * from './FilterForm';
+export { default } from './FilterForm';
